17dc17de9a26fac9e1d1c6ea59d8748c15111353
Ranyanna Ayala <ranyanna-@hotmail.com>
2026-04-09 17:01:55 -0400

    feat: modal funcional, estilização pendente

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,9 @@
 const grid = document.querySelector('.grid')
+const modalContainer = document.querySelector('.modal-container')
+const modalNome = document.querySelector('#modal-nome')
+const modalDescricao = document.querySelector('#descricao')
+const modalLink = document.querySelector('#modal-link')
+const modalImg = document.querySelector('.projeto')
 
 projects.forEach(project => {
     const card = document.createElement('div')
@@ -7,7 +12,19 @@ projects.forEach(project => {
     const img = document.createElement('img')
     img.src = project.imagem
 
+    card.addEventListener('click', () => {
+        modalContainer.classList.remove('hidden')
+        modalNome.textContent = project.nome
+        modalDescricao.textContent = project.descricao
+        modalLink.href = project.link
+        modalImg.src = project.imagem
+    })
+
     card.appendChild(img)
     grid.appendChild(card)
 })
 
+const close = document.querySelector('#close')
+close.addEventListener('click', () => {
+    modalContainer.classList.add('hidden')
+})
